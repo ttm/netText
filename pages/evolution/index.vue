2144779583564afc8ded281d616cf966601bed05
Renato Fabbri <renato.fabbri@gmail.com>
2019-03-26 18:32:36 -0300

    incorporadas linhas com hip

--- pages/evolution/index.vue
+++ pages/evolution/index.vue
@@ -273,6 +273,7 @@
       box
       label="information about the network (editable)"
     >statistics:</textarea>
+    <svg></svg>
   </v-container>
 </template>
 
@@ -283,6 +284,7 @@ const enet = require('~/static/here.json')
 // const enet = JSON.parse(fs.readFileSync('~/static/here.json', 'utf8'))
 import * as BABYLON from 'babylonjs'
 import $ from 'jquery'
+import * as d3 from 'd3'
 
 function mkArrow(v1, v2) {
   let theta = Math.PI * 5 / 6
@@ -354,6 +356,105 @@ export default {
     }
   },
   methods: {
+    mkLineChart () {
+      var margin = {top: 50, right: 350, bottom: 50, left: 50}
+        , width = window.innerWidth - margin.left - margin.right // Use the window's width 
+        , height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
+
+      // The number of datapoints
+      var n = this.net_snapshots.networks.length
+
+      let self = this
+
+      // 5. X scale will use the index of our data
+      var xScale = d3.scaleLinear()
+          .domain([0, n-1]) // input
+          .range([0, width]) // output
+
+      // 6. Y scale will use the randomly generate number 
+      var yScale = d3.scaleLinear()
+          .domain([0, 1]) // input 
+          .range([height, 0]) // output 
+
+
+      // // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
+      // var dataset = d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
+
+      // 7. d3's line generator
+      var line = d3.line()
+          .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
+          .y(function(d) { return yScale(d); }) // set the y values for the line generator 
+          .curve(d3.curveMonotoneX) // apply smoothing to the line
+      // 1. Add the SVG to the page and employ #2
+      var svg = d3.select("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+      // 3. Call the x axis in a group tag
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(xScale)) // Create an axis component with d3.axisBottom
+
+      // 4. Call the y axis in a group tag
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+
+      // 9. Append the path, bind the data, and call the line generator 
+      svg.append("path")
+          .datum(self.hubs_timeline) // 10. Binds data to the line 
+          .attr("class", "line") // Assign a class for styling 
+          .attr("d", line); // 11. Calls the line generator 
+
+      svg.append("path")
+          .datum(self.inter_timeline) // 10. Binds data to the line 
+          .attr("class", "line2") // Assign a class for styling 
+          .attr("d", line); // 11. Calls the line generator 
+
+      svg.append("path")
+          .datum(self.per_timeline) // 10. Binds data to the line 
+          .attr("class", "line3") // Assign a class for styling 
+          .attr("d", line); // 11. Calls the line generator 
+
+      // 12. Appends a circle for each datapoint 
+      svg.selectAll(".dot")
+        .data(self.hubs_timeline)
+        .enter().append("circle") // Uses the enter().append() method
+          .attr("class", "dot") // Assign a class for styling
+          .attr("cx", function(d, i) { return xScale(i) })
+          .attr("cy", function(d) { return yScale(d) })
+          .attr("r", 5)
+            .on("mouseover", function(a, b, c) { 
+              console.log('hub', a) 
+            })
+            .on("mouseout", function() {  }) 
+
+      svg.selectAll(".dot2")
+        .data(self.inter_timeline)
+        .enter().append("circle") // Uses the enter().append() method
+          .attr("class", "dot2") // Assign a class for styling
+          .attr("cx", function(d, i) { return xScale(i) })
+          .attr("cy", function(d) { return yScale(d) })
+          .attr("r", 5)
+            .on("mouseover", function(a, b, c) { 
+              console.log('hub', a) 
+            })
+            .on("mouseout", function() {  }) 
+      svg.selectAll(".dot3")
+        .data(self.per_timeline)
+        .enter().append("circle") // Uses the enter().append() method
+          .attr("class", "dot3") // Assign a class for styling
+          .attr("cx", function(d, i) { return xScale(i) })
+          .attr("cy", function(d) { return yScale(d) })
+          .attr("r", 5)
+            .on("mouseover", function(a, b, c) { 
+              console.log('hub', a) 
+            })
+            .on("mouseout", function() {  }) 
+    },
     loadSettings (set) {
       this.setting = set
       this.sname = set.name
@@ -401,7 +502,24 @@ export default {
         this.textinfo.value += '\n~ picked stats ~'
         console.log('post returned', data) 
         this.loadBabylon()
+        this.mkTimelineData()
+        this.mkLineChart()
       })
+    },
+    mkTimelineData () {
+      this.hubs_timeline = []
+      this.inter_timeline = []
+      this.per_timeline = []
+      for (let i = 1; i < this.net_snapshots.stats.length; i++) {
+        let hip = this.net_snapshots.stats[i].hip
+        let total = hip[0].length + hip[1].length + hip[2].length
+        let h_frac = hip[0].length / total
+        let i_frac = hip[1].length / total
+        let p_frac = hip[2].length / total
+        this.hubs_timeline.push(h_frac)
+        this.inter_timeline.push(i_frac)
+        this.per_timeline.push(p_frac)
+      }
     },
     absorbNetworksData (data) {
       // data.transactions = data.transactions.slice(0,100)
@@ -812,6 +930,7 @@ export default {
     }
   },
   mounted () {
+    window.md3 = d3
     this.loadNetwork(this.network)
     //this.loadBabylon()
     window.__this = this
@@ -844,10 +963,53 @@ export default {
   }
 }
 </script>
+
 <style>
 #renderCanvas {
     width   : 50%;
     height  : 50%;
     touch-action: none;
+}
+.line {
+    fill: none;
+    stroke: #ffab00;
+    stroke-width: 3;
+}
+
+.line2 {
+    fill: none;
+    stroke: #00abff;
+    stroke-width: 3;
+}
+
+.line3 {
+    fill: none;
+    stroke: #00ffab;
+    stroke-width: 3;
+}
+  
+.overlay {
+  fill: none;
+  pointer-events: all;
+}
+
+/* Style the dots by assigning a fill and stroke */
+.dot {
+    fill: #ffab00;
+    stroke: #fff;
+}
+
+.dot2 {
+    fill: #00abff;
+    stroke: #fff;
+}
+  
+.dot3 {
+    fill: #00ffab;
+    stroke: #fff;
+}
+.focus circle {
+  fill: none;
+  stroke: steelblue;
 }
 </style>
