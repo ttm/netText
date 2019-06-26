19f8f8657a0f024ebe204b0169631a34c1227c33
Renato Fabbri <renato.fabbri@gmail.com>
2019-06-25 22:22:15 -0300

    terminada versao inicial topdown2

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
     <v-btn class="green" to="communicability">Communicability</v-btn>
     <v-btn class="orange" to="multilevel2/interpolated">Multilevel 2D interpolated</v-btn>
     <v-btn class="teal" to="multilevel2/topdown">Multilevel 2D topdown</v-btn>
+    <v-btn class="brown" to="multilevel2/topdown2">Multilevel 2D topdown v2</v-btn>
     <v-btn class="yellow" to="pixiii">mpixi</v-btn>
   </div>
 </template>
