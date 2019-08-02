7a65d5d689b5084c627cb3e2dc2abbddd3c63665
Renato Fabbri <renato.fabbri@gmail.com>
2019-08-02 05:44:11 -0300

    adicionado arquivo p tempo

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,21 @@
   </v-app>
 </template>
 
+<script>
+export default {
+  head () {
+    return {
+      script: [
+        { src: "https://www.googletagmanager.com/gtag/js?id=UA-145065993-1" },
+      ],
+      meta: [
+        { name: "google-site-verification", content: "_7QyOvNFeTE-F03YCYZWs15DeiokzdL7ykVdJqy-3hk" }
+      ]
+    }
+  },
+}
+</script>
+
 <style>
 html, body {
   overflow: scroll;
