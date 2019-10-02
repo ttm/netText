5e6df139bf966d1f43c9f5a2492691fcf5756df7
Renato Fabbri <renato.fabbri@gmail.com>
2019-10-02 18:11:09 -0300

    sonzeira antes de colocar coisa no canvas - mynsa

--- pages/index.vue
+++ pages/index.vue
@@ -11,3 +11,41 @@
     <v-btn class="yellow" to="pixiii">mpixi</v-btn>
   </div>
 </template>
+
+<script>
+import Tone from 'tone'
+
+export default {
+  data () {
+    return {
+      something: 10,
+    }
+  },
+  methods: {
+    sporkThing () {
+      this.synth.triggerAttackRelease('C4', 0.5, 0)
+      this.synth.triggerAttackRelease('E4', 0.5, 1)
+      this.synth.triggerAttackRelease('G4', 0.5, 2)
+      this.synth.triggerAttackRelease('B4', 0.5, 3)
+      this.synth2.triggerAttackRelease('C5', 0.5, 4)
+      this.synth3.triggerAttackRelease('E4', 0.3, 4)
+      this.synth4.triggerAttackRelease('G4', 0.6, 4)
+      this.synth5.triggerAttackRelease('C4', 0.8, 5)
+      this.synth.triggerAttackRelease('B4', 0.5, 4.5)
+      this.synth.triggerAttackRelease('A4', 0.7, 5)
+    },
+    initSynths () {
+      this.synth = new Tone.Synth().toMaster()
+      this.synth2 = new Tone.Synth().toMaster()
+      this.synth3 = new Tone.Synth().toMaster()
+      this.synth4 = new Tone.Synth().toMaster()
+      this.synth5 = new Tone.Synth().toMaster()
+    },
+  },
+  mounted () {
+    window.__this = this
+    this.initSynths()
+    this.sporkThing()
+  },
+}
+</script>
